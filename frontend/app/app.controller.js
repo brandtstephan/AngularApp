58f18c66dc43748a78f363fa65e75a2707706bfd
DESKTOP-EV9DUQ7\Buritomaster <stephan_000@hotmail.com>
2018-07-22 17:12:05 +0200

    fixed d3, directive still not working

--- frontend/app/app.controller.js
+++ frontend/app/app.controller.js
@@ -2,85 +2,6 @@
   "use strict";
   angular
     .module("app")
-    .directive("dataChart", function (d3) {
-      return {
-        restrict: "E",
-        template: '<svg id="chart" width="960" height="500"></svg>',
-        link: function ($scope, element, attrs) {
-          var data = [
-            { name: "Javascript", value: "7" },
-            { name: "Java", value: "7" },
-            { name: "C++", value: "3" },
-            { name: "HTML", value: "5" },
-            { name: "CSS", value: "5" },
-            { name: "Python", value: "5" },
-            { name: "C#", value: "6" }
-          ];
-          console.log("hello");
-          var svg = d3.select("svg"),
-            margin = { top: 20, right: 20, bottom: 30, left: 40 },
-            width = +svg.attr("width") - margin.left - margin.right,
-            height = +svg.attr("height") - margin.top - margin.bottom;
-
-          var x = d3
-            .scaleBand()
-            .rangeRound([0, width])
-            .padding(0.1),
-            y = d3.scaleLinear().rangeRound([height, 0]);
-
-          var g = svg
-            .append("g")
-            .attr(
-              "transform",
-              "translate(" + margin.left + "," + margin.top + ")"
-            );
-
-          x.domain(
-            data.map(function (d) {
-              return d.name;
-            })
-          );
-          y.domain([
-            0,
-            d3.max(data, function (d) {
-              return d.value;
-            })
-          ]);
-
-          g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-          g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(10, "%"))
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Frequency");
-
-          g.selectAll(".bar")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("class", "bar")
-            .attr("x", function (d) {
-              return x(d.name);
-            })
-            .attr("y", function (d) {
-              return y(d.value);
-            })
-            .attr("width", x.bandwidth())
-            .attr("height", function (d) {
-              return height - y(d.value);
-            });
-
-        }
-      };
-    })
     .factory("User", function () {
       const users = [
         {
@@ -147,5 +68,10 @@
       console.log($scope.groups);
 
       $scope.radioModel = "Left";
+    }).directive("dataChart", function () {
+      return {
+        restrict: "E",
+        templateUrl: "d3-template.html"
+      };
     });
 })();
